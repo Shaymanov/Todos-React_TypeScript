4e70b652d278ca5407aef5ad4b3239bff2c0a326
Muhammad <magomed.shaymanov2015@yandex.ru>
2025-10-24 05:54:01 +0300

    created a todo-app in React

---
+++ src/components/Form/Form.tsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './Form.scss'
+
+export const Form = (props: { createNewToDo: Function }) => {
+  const [text, setText] = useState<string>('')
+
+  const formSubmit = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    if (text) {
+      props.createNewToDo(text)
+      setText('')
+    }
+  }
+
+  return (
+    <div className="form-wrapper">
+      <form action="#" onSubmit={formSubmit}>
+        <label>
+          <input
+            type="text"
+            value={text}
+            onChange={(event) => setText(event.target.value)}
+          />
+          <button></button>
+        </label>
+      </form>
+    </div>
+  )
+}
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+import classes from './Header.module.scss'
+import { NavLink } from 'react-router-dom'
+
+export const Header = () => {
+  const getActiveClass = ({ isActive }: { isActive: boolean }): string => {
+    return isActive ? `${classes.active} ${classes.link}` : `${classes.link}`
+  }
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.container}>
+        <NavLink to="/" className={getActiveClass}>
+          ToDo
+        </NavLink>
+        <NavLink to="/list" className={getActiveClass}>
+          List
+        </NavLink>
+      </div>
+    </header>
+  )
+}
